ac4010cbf9cbae406f5f78a04857f27ea7a92c4c
Jose Martins <lluismmartins7@gmail.com>
2017-02-26 17:44:00 +0000

    added side bar

--- templates/footer.php
+++ templates/footer.php
@@ -1,2 +1,23 @@
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+
+    <!-- Menu Toggle Script -->
+    <script>
+      $("#menu-toggle").click(function(e) {
+          e.preventDefault();
+          $("#wrapper").toggleClass("toggled");
+      });
+    </script>
   </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -7,6 +7,7 @@
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
   <title>LBAW Project</title>
 
+  <link rel="stylesheet" href="../css/style.css">
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
@@ -15,11 +16,13 @@
 
   <!-- Latest compiled JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
 </head>
 <body>
 
 
-  <nav class="navbar navbar-inverse">
+  <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
@@ -29,15 +32,44 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">Consord</a>
+
+
+
+        <a class="navbar-brand" href="#" id="menu-toggle" >Consord</a>
       </div>
 
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav navbar-right">
-          <li class="active navbar-right"><a href="#">Mario <span class="sr-only">(current)</span></a></li>
+          <li class="active navbar-right"><a href="#">Mario</a></li>
+
+
         </ul>
 
       </div>
     </div>
   </nav>
+
+
+  <div id="wrapper">
+
+          <!-- Sidebar -->
+          <div id="sidebar-wrapper">
+              <ul class="sidebar-nav">
+                  <li>
+                      <a href="#">Project 1</a>
+                  </li>
+                  <li>
+                      <a href="#">Project 2</a>
+                  </li>
+                  <li>
+                      <a href="#">Project 3</a>
+                  </li>
+          </div>
+          <!-- /#sidebar-wrapper -->
+
+          <!-- Page Content -->
+          <div id="page-content-wrapper">
+              <div class="container-fluid">
+                  <div class="row">
+                      <div class="col-lg-12">
