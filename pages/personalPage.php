7c79c271a208db44586e24aea172ea0ba9f8a0c9
Jose Martins <lluismmartins7@gmail.com>
2017-02-26 20:16:03 +0000

    minor fixes

--- pages/personalPage.php
+++ pages/personalPage.php
@@ -9,8 +9,7 @@ include_once '../templates/header.php';
     <h2> My Projects </h2>
   </div>
 
-
-  <div class="card col-lg-3 col-md-3 col-sm-6 col-xs-12" style="width: 20rem; background:grey;">
+  <div class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
     <div class="card-block">
       <h3 class="card-title">Special title treatment</h3>
       <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -18,7 +17,7 @@ include_once '../templates/header.php';
     </div>
   </div>
 
-  <div class="card col-lg-3 col-md-3 col-sm-6 col-xs-12" style="width: 20rem; background:grey;">
+  <div class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
     <div class="card-block">
       <h3 class="card-title">Special title treatment</h3>
       <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -26,7 +25,7 @@ include_once '../templates/header.php';
     </div>
   </div>
 
-  <div class="card col-lg-3 col-md-3 col-sm-6 col-xs-12" style="width: 20rem; background:grey;">
+  <div class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
     <div class="card-block">
       <h3 class="card-title">Special title treatment</h3>
       <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -34,7 +33,7 @@ include_once '../templates/header.php';
     </div>
   </div>
 
-  <div class="card col-lg-3 col-md-3 col-sm-6 col-xs-12" style="width: 20rem; background:grey;">
+  <div class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
     <div class="card-block">
       <h3 class="card-title">Special title treatment</h3>
       <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -42,7 +41,24 @@ include_once '../templates/header.php';
     </div>
   </div>
 
-  <div class="card col-lg-3 col-md-3 col-sm-6 col-xs-12" style="width: 20rem; background:grey;">
+  <div class="card col-lg-3 col-md-3 col-sm-6 col-xs-12" >
+    <div class="card-block">
+      <h3 class="card-title">Add Project</h3>
+          <a href="#">
+            <i class="fa fa-plus fa-5x"></i>
+          </a>
+    </div>
+  </div>
+
+</div>
+
+<div class="container">
+  <div class="page-header">
+    <h2> My Collaborations </h2>
+  </div>
+
+
+  <div class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
     <div class="card-block">
       <h3 class="card-title">Special title treatment</h3>
       <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -53,21 +69,6 @@ include_once '../templates/header.php';
 
 </div>
 
-<div class="container">
-  <div class="page-header">
-    <h2> My Collaborations </h2>
-  </div>
-
-
-
-  <div class="card col-lg-3 col-md-3 col-sm-6 col-xs-12" style="width: 20rem; background:grey;">
-    <div class="card-block">
-      <h3 class="card-title">Special title treatment</h3>
-      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-  </div
 
 
 
