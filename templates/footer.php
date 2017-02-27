d33a275d52c88d76fc5a68a40101968a4b1cdc37
David Joel Nogueira Azevedo <davidazevedo1996@hotmail.com>
2017-02-27 17:34:10 +0000

    Added progress bar

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,8 @@
 
+<footer class="footer">
+    <div class="container text-center ">
+        <p>Copyright <i class="glyphicon glyphicon-copyright-mark"></i> ICN</p>
+    </div>
+</footer>
   </body>
 </html>
