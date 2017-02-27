9987af2a359c0edde30694a2f2dc26fe3b3033c2
David Joel Nogueira Azevedo <davidazevedo1996@hotmail.com>
2017-02-27 18:02:12 +0000

    Finished projectBody.php

--- templates/projectBody.php
+++ templates/projectBody.php
@@ -1,13 +1,181 @@
 <div class="col-sm-8 col-xs-12">
-  <div class="color-blue">
-    PAGE-CONTENT
-    <ul>
-      <li>Center</li>
-      <li>Center</li>
-      <li>Center</li>
-      <li>Center</li>
-      <li>Center</li>
-      <li>Center</li>
-    </ul>
-  </div>
+    <h2 class="text-center bottom">Project Title</h2>
+    <div class="container">
+        <p class="text-justify well">Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida est sed tortor pretium, et sollicitudin lacus aliquet. Mauris vel magna nec lectus pulvinar congue eu sit amet enim. Proin ultrices commodo odio vestibulum tempor. Donec auctor dolor vel lobortis feugiat. Suspendisse sit amet congue orci, sit amet fermentum ipsum. Suspendisse quis lectus eget enim scelerisque pellentesque. Sed a malesuada ipsum. </p>
+    </div>
+    <div class="container">
+    	<div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <div id="imaginary_container">
+                    <div class="input-group stylish-input-group">
+                        <input type="text" class="form-control"  placeholder="Search" >
+                        <span class="input-group-addon">
+                            <button type="submit">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+    	</div>
+    </div>
+    <div class="col-sm-8">
+  <!-- LINKS -->
+      <div class="container">
+        <div class="page-header">
+          <h2> Links </h2>
+        </div>
+
+        <div class="carousel slide" id="linkCarrousel">
+          <div class="carousel-inner">
+            <div class="item active"><!-- /Slide1 -->
+              <ul class="thumbnails">
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="item"><!-- /Slide2 -->
+              <ul class="thumbnails">
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <nav>
+            <ul class="control-box pager">
+              <li><a data-slide="prev" href="#linkCarrousel" class=""><i class="glyphicon glyphicon-chevron-left"></i></a></li>
+              <li><a data-slide="next" href="#linkCarrousel" class=""><i class="glyphicon glyphicon-chevron-right"></i></a></li>
+              </ul>
+            </nav>
+            <!-- /.control-box -->
+
+        </div><!-- /#linkCarrousel -->
+      </div>
+
+      <!-- Documents -->
+      <div class="container">
+        <div class="page-header">
+          <h2> Documents </h2>
+        </div>
+
+
+        <div class="carousel slide" id="documentsCarrousel">
+          <div class="carousel-inner">
+            <div class="item active"><!-- /Slide1 -->
+              <ul class="thumbnails">
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="item"><!-- /Slide2 -->
+              <ul class="thumbnails">
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+                <li class="card col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                  <div class="card-block">
+                    <h3 class="card-title">Special title treatment</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <nav>
+            <ul class="control-box pager">
+              <li><a data-slide="prev" href="#documentsCarrousel" class=""><i class="glyphicon glyphicon-chevron-left"></i></a></li>
+              <li><a data-slide="next" href="#documentsCarrousel" class=""><i class="glyphicon glyphicon-chevron-right"></i></a></li>
+              </ul>
+            </nav>
+            <!-- /.control-box -->
+        </div><!-- /#documentsCarrousel -->
+      </div>
+    </div><!--/.col-sm-10-->
 </div>
