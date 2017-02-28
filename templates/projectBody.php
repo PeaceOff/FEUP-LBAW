f18259b14b8971f77d17871ec528dee514d637dc
David Joel Nogueira Azevedo <davidazevedo1996@hotmail.com>
2017-02-28 11:34:54 +0000

    Changed links div

--- templates/projectBody.php
+++ templates/projectBody.php
@@ -23,178 +23,172 @@
         <!-- LINKS -->
         <div class="container">
             <div class="page-header">
-                <h2> Links </h2>
+                <h3> Links </h3>
             </div>
 
-            <div class="carousel slide" id="linkCarrousel">
-                <div class="carousel-inner">
-                    <div class="item active"><!-- /Slide1 -->
-                        <ul class="thumbnails">
-                            <li class="card colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <img class="img-responsive img-circle" alt="an image" src="../assets/facebook.jpg" />
-                                </div>
-                            </li>
+            <div id="linkCarrousel" style="overflow-x : scroll;">
+                <ul class="thumbnails">
+                    <li class="card card-small colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                        <div class="card-block">
+                            <a href="https://facebook.com">
+                            <img class="img-responsive img-circle" alt="an image" src="../assets/facebook.jpg" />
+                            </a>
+                        </div>
+                    </li>
 
-                            <li class="card colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <img class="img-responsive img-circle" alt="an image" src="../assets/github.png" />
-                                </div>
-                            </li>
-                            <li class="card colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <img class="img-responsive img-circle" alt="an image" src="../assets/google.jpg" />
-                                </div>
-                            </li>
-                            <li class="card colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <img class="img-responsive img-circle" alt="an image" src="../assets/trello.png" />
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="item"><!-- /Slide2 -->
-                        <ul class="thumbnails">
-                            <li class="card colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <img class="img-responsive img-circle" alt="an image" src="../assets/youtube.jpg" />
-                                </div>
-                            </li>
-                            <li class="card colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <img class="img-responsive img-circle" alt="an image" src="../assets/twitter.png" />
-                                </div>
-                            </li>
-                            <li class="card colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <img class="img-responsive img-circle" alt="an image" src="../assets/feup.png" />
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                    <li class="card card-small colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                        <div class="card-block">
+                            <a href="https://github.com">
+                            <img class="img-responsive img-circle" alt="an image" src="../assets/github.png" />
+                            </a>
+                        </div>
+                    </li>
+                    <li class="card card-small colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                        <div class="card-block">
+                            <a href="https://google.com">
+                            <img class="img-responsive img-circle" alt="an image" src="../assets/google.jpg" />
+                            </a>
+                        </div>
+                    </li>
+                    <li class="card card-small colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                        <div class="card-block">
+                            <a href="https://trello.com">
+                            <img class="img-responsive img-circle" alt="an image" src="../assets/trello.png" />
+                            </a>
+                        </div>
+                    </li>
+                    <li class="card card-small colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                        <div class="card-block">
+                            <a href="https://youtube.com">
+                            <img class="img-responsive img-circle" alt="an image" src="../assets/youtube.jpg" />
+                            </a>
+                        </div>
+                    </li>
+                    <li class="card card-small colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                        <div class="card-block">
+                            <a href="https://twitter.com">
+                            <img class="img-responsive img-circle" alt="an image" src="../assets/twitter.png" />
+                            </a>
+                        </div>
+                    </li>
+                    <li class="card card-small colorless col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                        <div class="card-block">
+                            <a href="https://fe.up.pt">
+                            <img class="img-responsive img-circle" alt="an image" src="../assets/feup.png" />
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div><!-- /#linkCarrousel -->
 
-                <nav>
-                    <ul class="control-box pager">
-                        <li><a data-slide="prev" href="#linkCarrousel" class=""><i class="glyphicon glyphicon-chevron-left"></i></a></li>
-                        <li><a data-slide="next" href="#linkCarrousel" class=""><i class="glyphicon glyphicon-chevron-right"></i></a></li>
+        <div class="page-header">
+            <h3> Documents </h3>
+        </div>
+
+
+        <div class="carousel slide" id="documentsCarrousel">
+            <div class="carousel-inner">
+                <div class="item active"><!-- /Slide1 -->
+                    <ul class="thumbnails">
+                        <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                            <div class="card-block">
+                                <h3 class="card-title">Special title treatment</h3>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </li>
+
+                        <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                            <div class="card-block">
+                                <h3 class="card-title">Special title treatment</h3>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </li>
+                        <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                            <div class="card-block">
+                                <h3 class="card-title">Special title treatment</h3>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </li>
+                        <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                            <div class="card-block">
+                                <h3 class="card-title">Special title treatment</h3>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </li>
                     </ul>
-                </nav>
-                <!-- /.control-box -->
-
-            </div><!-- /#linkCarrousel -->
-        </div>
-
-        <!-- Documents -->
-        <div class="container">
-            <div class="page-header">
-                <h2> Documents </h2>
-            </div>
-
-
-            <div class="carousel slide" id="documentsCarrousel">
-                <div class="carousel-inner">
-                    <div class="item active"><!-- /Slide1 -->
-                        <ul class="thumbnails">
-                            <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <h3 class="card-title">Special title treatment</h3>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </li>
-
-                            <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <h3 class="card-title">Special title treatment</h3>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </li>
-                            <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <h3 class="card-title">Special title treatment</h3>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </li>
-                            <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <h3 class="card-title">Special title treatment</h3>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="item"><!-- /Slide2 -->
-                        <ul class="thumbnails">
-                            <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <h3 class="card-title">Special title treatment</h3>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </li>
-                            <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <h3 class="card-title">Special title treatment</h3>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </li>
-                            <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
-                                <div class="card-block">
-                                    <h3 class="card-title">Special title treatment</h3>
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
-
-                <nav>
-                    <ul class="control-box pager">
-                        <li><a data-slide="prev" href="#documentsCarrousel" class=""><i class="glyphicon glyphicon-chevron-left"></i></a></li>
-                        <li><a data-slide="next" href="#documentsCarrousel" class=""><i class="glyphicon glyphicon-chevron-right"></i></a></li>
+                <div class="item"><!-- /Slide2 -->
+                    <ul class="thumbnails">
+                        <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                            <div class="card-block">
+                                <h3 class="card-title">Special title treatment</h3>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </li>
+                        <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                            <div class="card-block">
+                                <h3 class="card-title">Special title treatment</h3>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </li>
+                        <li class="card col-lg-8 col-md-8 col-sm-10 col-xs-12">
+                            <div class="card-block">
+                                <h3 class="card-title">Special title treatment</h3>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </li>
                     </ul>
-                </nav>
-                <!-- /.control-box -->
-            </div><!-- /#documentsCarrousel -->
-
-        </div>
-
-        <div class="container">
-            <div class="page-header">
-                <h2> Todo </h2>
-            </div>
-
-            <div class="jumbotron">
-                <a  href="todoPage.php" > <img src="../assets/todo.jpg" class="img-responsive img-circle center-block" width="200"> </a>
-            </div>
-
-
-        </div>
-
-        <!-- Progress Bar -->
-        <div class="container">
-            <div class="progress" id="progress-bar-div">
-                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-                <span class="sr-only">60% Complete</span>
                 </div>
             </div>
-        </div>
-        <script>
 
-            $(document).ready(function () {
+            <nav>
+                <ul class="control-box pager">
+                    <li><a data-slide="prev" href="#documentsCarrousel" class=""><i class="glyphicon glyphicon-chevron-left"></i></a></li>
+                    <li><a data-slide="next" href="#documentsCarrousel" class=""><i class="glyphicon glyphicon-chevron-right"></i></a></li>
+                </ul>
+            </nav>
+            <!-- /.control-box -->
+        </div><!-- /#documentsCarrousel -->
 
-                var percent = 70;
-                $("#progress-bar-div").children(".progress-bar").attr("aria-valuenow",percent);
-                $("#progress-bar-div").children(".progress-bar").width(percent+"%");
-                $("#progress-bar-div").children(".progress-bar").children(".sr-only").html(percent + "% complete");
-            });
-
-        </script>
     </div>
+
+    <div class="container">
+        <div class="page-header">
+            <h2> Todo </h2>
+        </div>
+
+        <div class="jumbotron">
+            <a  href="todoPage.php" > <img src="../assets/todo.jpg" class="img-responsive img-circle center-block" width="200"> </a>
+        </div>
+
+
+    </div>
+
+    <!-- Progress Bar -->
+    <div class="container">
+        <div class="progress" id="progress-bar-div">
+            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                <span class="sr-only">60% Complete</span>
+            </div>
+        </div>
+    </div>
+    <script>
+
+    $(document).ready(function () {
+
+        var percent = 70;
+        $("#progress-bar-div").children(".progress-bar").attr("aria-valuenow",percent);
+        $("#progress-bar-div").children(".progress-bar").width(percent+"%");
+        $("#progress-bar-div").children(".progress-bar").children(".sr-only").html(percent + "% complete");
+    });
+
+    </script>
 </div>
