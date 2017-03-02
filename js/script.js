df472a6b4e0b71a81d6a7cb38ff2a35d58b357a8
mferreira96 <pount@sapo.pt>
2017-03-02 08:33:37 +0000

    cleaning some unusable stuff

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-$(function() {
-    $('#signInLink').click(function(e) {
-    $("#signIn-form").delay(100).fadeIn(100);
- 		$("#signUp-form").fadeOut(100);
-    $("#signUpLink").removeClass('selected');
-    $(this).addClass('selected');
-  	e.preventDefault();
-	});
-
-	  $('#signUpLink').click(function(e) {
-		$("#signUp-form").delay(100).fadeIn(100);
- 		$("#signIn-form").fadeOut(100);
-    $("#signInLink").removeClass('selected');
-    $(this).addClass('selected');
-  	e.preventDefault();
-	});
-
-});
-
-
 function addShow(index){
   var target = $(this).attr("target");
 
