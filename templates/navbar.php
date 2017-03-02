630d7c804bc87b77fdab225b3125e8d1374212d5
Jose Martins <lluismmartins7@gmail.com>
2017-03-02 09:52:42 +0000

    refactoring

--- templates/navbar.php
+++ templates/navbar.php
@@ -15,72 +15,12 @@
       <ul class="nav navbar-nav navbar-right">
 
         <li class="active"><a href="personalPage.php">Mario</a></li>
+
         <li>
-
-        <!-- notifications -->
-
-          <a class="btn btn-lg dropdown-toggle"  id="dLabel" role="button" data-toggle="dropdown" >
-            <i class="fa fa-bell" aria-hidden="true"></i>
-            <span class="badge badge-notify"> 4 </span>
-          </a>
-
-          <ul class="dropdown-menu notifications" role="menu" aria-labelledby="dLabel">
-
-            <div class="notification-heading">
-              <h4 class="notifications-title">Notifications</h4>
-              <a href="#"> <h4 class="notifications-title pull-right"> Dismmiss all   <i class="fa fa-arrow-right" aria-hidden="true"></i></h4> </a>
-            </div>
-
-            <li class="divider"></li>
-
-            <div class="notifications-wrapper">
-
-                <div class="notification-item">
-                  <h4 class="item-title">David Azevedo Invited you to join LBAW-A3</h4>
-                  <p class="item-info">27-02-2017, one day ago</p>
-                  <div class="notification-links pull-right">
-                    <a><i class="fa fa-check-circle fa-2x" aria-hidden="true"></i></a>
-                    <a><i class="fa fa-times-circle fa-2x" aria-hidden="true"></i></a>
-                  </div>
-                </div>
-
-
-                <div class="notification-item">
-                  <h4 class="item-title">Marcelo Ferreira assigned you to a task on LBAW-A2</h4>
-                  <p class="item-info">27-02-2017, one day ago</p>
-                <div class="notification-links pull-right">
-                      <a><i class="fa fa-check fa-2x" aria-hidden="true"></i>  </a>
-                  </div>
-                </div>
-
-
-                <div class="notification-item">
-                  <h4 class="item-title">Joao Ferreira assigned you to a task on LBAW-A2</h4>
-                  <p class="item-info">27-02-2017, one day ago</p>
-                  <div class="notification-links pull-right">
-                      <a><i class="fa fa-check fa-2x" aria-hidden="true"></i>  </a>
-                  </div>
-                </div>
-
-
-                <div class="notification-item">
-                  <h4 class="item-title">David Azevedo Invited you to join PPIN</h4>
-                  <p class="item-info">27-02-2017, one day ago</p>
-                  <div class="notification-links pull-right">
-                    <a><i class="fa fa-check-circle fa-2x" aria-hidden="true"></i></a>
-                    <a><i class="fa fa-times-circle fa-2x" aria-hidden="true"></i></a>
-                  </div>
-                </div>
-
-            </div>
-
-            <li class="divider"></li>
-
-          </ul>
-
-
-
-      </li>
+          <?php
+            include_once '../templates/notifications.php';
+          ?>
+        </li>
 
       <li ><a href="#">Logout</a></li>
 
