988beff7af3a839ce5cc4bb721edfc785b116d7e
mferreira96 <pount@sapo.pt>
2017-03-02 17:11:36 +0000

    fixed bug

--- js/script.js
+++ js/script.js
@@ -33,5 +33,5 @@ $(document).ready(function(){
 });
 
 
-Dropzone.options.addFileForm.autoProcessQueue = false; // this way files we only be uploaded whrn we call myDropzone.processQueue()
+Dropzone.options.autoProcessQueue = false; // this way files we only be uploaded whrn we call myDropzone.processQueue()
 Dropzone.options.addRemoveLinks = true;
