29442f65ad17c4cee50f6270a85cf9f33c468904
David Joel Nogueira Azevedo <davidazevedo1996@hotmail.com>
2017-03-04 11:03:32 +0000

    Updated right side bar for mobile

--- templates/projectBody.php
+++ templates/projectBody.php
@@ -25,6 +25,63 @@
     </div>
   </div>
 
+
+  <!-- Project Management -->
+  <div class="container visible-xs">
+    <div class="page-header">
+      <h3 style="margin : auto">Project Management
+      <button type="button" class="btn btn-default btn-sm" data-toggle="collapse" data-target="#management" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="caret"></span>
+      </button>
+      </h3>
+    </div>
+    <div class="collapse" id="management">
+      <ul class="nav nav-tabs">
+        <li class="nav-item active">
+          <a class="nav-link color-blue" href="#">Users</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Add User</a>
+        </li>
+      </ul>
+      <table class="table table-condensed table-style">
+        <tr>
+          <td>
+            User A
+          </td>
+          <td class="align-right">
+            <button href="" class="btn btn-danger btn-xs"> Remove </button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            User B
+          </td>
+          <td class="align-right">
+            <button href="" class="btn btn-danger btn-xs"> Remove </button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            User C
+          </td>
+          <td class="align-right">
+            <button href="" class="btn btn-danger btn-xs"> Remove </button>
+          </td>
+        </tr>
+      </table>
+      <div class="width-100">
+        <ul class="btn-group-vertical width-100 padding-0">
+          <li class="btn btn-primary" data-toggle="modal" data-target="#forum1">Forum Topic 1</li>
+          <li class="btn btn-primary" data-toggle="modal" data-target="#forum1">Forum Topic 2</li>
+          <li class="btn btn-primary" data-toggle="modal" data-target="#forum1">Forum Topic 3</li>
+        </ul>
+      </div>
+      <?php include_once '../templates/forumExample.php'; ?>
+    </div>
+  </div>
+
   <!-- LINKS -->
   <div class="container">
     <div class="page-header">
@@ -162,3 +219,4 @@
 
                       </script>
                     </div>
+                  </div>
