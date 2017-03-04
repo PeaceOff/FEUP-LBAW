e1b640b080068c66dd711bf2b7a75ae81bb95e92
David Joel Nogueira Azevedo <davidazevedo1996@hotmail.com>
2017-03-04 15:16:04 +0000

    Added project deadline

--- templates/projectBody.php
+++ templates/projectBody.php
@@ -206,7 +206,8 @@
                       </div>
 
                       <!-- Progress Bar -->
-                      <div class="container">
+                      <div class="container text-center">
+                        Deadline : 24-10-2017
                         <div class="progress" id="progress-bar-div">
                           <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
                             <span class="sr-only">60% Complete</span>
