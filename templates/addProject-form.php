4e35dc09696ccbb82d66d082801abcca4e9ce73b
mferreira96 <pount@sapo.pt>
2017-03-05 17:26:42 +0000

    chenged dropdown

--- templates/addProject-form.php
+++ templates/addProject-form.php
@@ -6,19 +6,21 @@
     </div>
     <div class="modal-body">
       <form  id ="addProject-form" action="" method="post" style="display:block">
+
         <div class="form-group ">
           <label for="projectName">Name</label>
           <input class="form-control" type="text" tabindex="1" id="projectName" name="projectName" value="" required="" autofocus="">
         </div>
-        <div class="form-group ">
-          <label for="folderList">Folder</label>
-          <input id = "folderList" list="folder" class="form-control"  tabindex="2" name="folder">
-          <datalist id="folder">
-            <option value="Folder1">
-            <option value="Folder2">
-            <option value="Folder3">
-          </datalist>
+
+        <div class="form-group input-group width-100">
+          <label for="sel">Folder</label>
+          <select class="form-control" id="sel" tabindex="2">
+            <option>Folder1</option>
+            <option>Folder2</option>
+            <option>Folder3</option>
+          </select>
         </div>
+
         <div class="input-group form-group width-100">
           <label for="descriptionArea">Description</label>
           <textarea class="form-control resizable-horizontal" id="descriptionArea" rows="3" tabindex="3"></textarea>
