e349c7e52b2e84c057358df7066fe90636df63f7
mferreira96 <pount@sapo.pt>
2017-03-05 18:17:41 +0000

    added editProject-form.php

--- templates/project-card.php
+++ templates/project-card.php
@@ -1,3 +1,7 @@
+<?php
+  include_once 'editProject-form.php';
+ ?>
+
 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
   <div class="card cardhandler">
     <div class="cardheader">
@@ -8,7 +12,7 @@
     <div class="desc"> Todavia, a competitividade nas transações comerciais pode nos levar a considerar a reestruturação da gestão inovadora da qual fazemos parte. Acima de tudo, é fundamental ressaltar que a contínua expansão de nossa atividade ainda não demonstrou convincentemente que vai participar na mudança dos conhecimentos estratégicos para atingir a excelência.</div>
 
   <div class="bottom">
-      <a class="btn icon-link btn-primary btn-twitter btn-sm" href="#">
+      <a class="btn icon-link btn-primary btn-twitter btn-sm" href="" data-toggle="modal" data-target ="#editProject">
         <i class="fa fa-pencil"></i>
       </a>
       <a class="btn icon-link btn-danger btn-sm" rel="publisher" href="#">
