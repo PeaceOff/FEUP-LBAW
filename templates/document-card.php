ac3a4ca5299cec510c9d27371e88214b4185797c
Jose Martins <lluismmartins7@gmail.com>
2017-03-05 12:29:56 +0000

    added document card

---
+++ templates/document-card.php
@@ -0,0 +1,16 @@
+<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+  <div class="card document">
+    <div class="">
+      <h2>Requirements</h2>
+    </div>
+    <img src="../assets/document-card.png" >
+    <div class="document-bottom">
+        <a class="btn icon-link btn-primary btn-twitter btn-sm" href="#">
+          <i class="fa fa-pencil"></i>
+        </a>
+        <a class="btn icon-link btn-danger btn-sm" rel="publisher" href="#">
+          <i class="fa fa-trash"></i>
+        </a>
+      </div>
+  </div>
+</div>
