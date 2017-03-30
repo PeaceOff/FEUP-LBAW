b8a7c0f14e6ac611a4c113f0c90428de977b631d
PeaceOff <davidazevedo1996@hotmail.com>
2017-03-30 09:04:45 +0100

    Changing struct

--- templates/footer.php
+++
@@ -1,8 +0,0 @@
-  </div>
-<footer class="footer">
-    <div class="container text-center ">
-        <p>Copyright <i class="glyphicon glyphicon-copyright-mark"></i> ICN</p>
-    </div>
-</footer>
-  </body>
-</html>
