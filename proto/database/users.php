cb59036577e8eae2b12432ed68c59f52a0637833
noobcsf <noobcsf@hotmail.com>
2017-04-14 16:16:22 +0100

    Added Project and Users DB functions

--- proto/database/users.php
+++ proto/database/users.php
@@ -1,11 +1,37 @@
 <?php
 
 
+//user
+function user_add($username, $password, $email) {
+  global $conn;
+  $stmt = $conn->prepare("INSERT INTO user (username, email, password) VALUES (?, ?, ?)");
+  $stmt->execute(array($username, $email, $password));
+}
 
+//folder
+function user_add_folder($folder_name, $owner) {
+  global $conn;
+  $stmt = $conn->prepare("INSERT INTO folder (name, username) VALUES (?, ?)");
+  $stmt->execute(array($folder_name, $owner));
+}
 
+function user_update_folder($id, $folder_name) {
+  global $conn;
+  $stmt = $conn->prepare("UPDATE folder SET name = ? WHERE id = ?");
+  $stmt->execute(array($folder_name, $id));
+}
 
+function user_delete_folder($id) {
+  global $conn;
+  $stmt = $conn->prepare("DELETE FROM folder WHERE id = ?");
+  $stmt->execute(array($id));
+}
 
-
-
+function user_get_folders($username){
+  global $conn;
+  $stmt = $conn->prepare("SELECT * FROM view_project_folder WHERE owner = ?");
+  $stmt->execute(array($username));
+  return $stmt->fetchAll();
+}
 
 ?>
