3efeb93e82ba1151fe67bb3a4ad4dc0394529549
mferreira96 <pount@sapo.pt>
2017-04-14 16:22:21 +0100

    added forum and todo DB functions

--- proto/database/forum.php
+++ proto/database/forum.php
@@ -1,11 +1,51 @@
 <?php
 
+function getTitleOfTopic($idProject){
+  global $conn;
 
+  $stmt = $conn->prepare("SELECT title FROM topic WHERE project_id = ?");
+  $stmt->execute(array($idProject));
+  return $stmt->fetchAll();
+}
 
+function getPostsOfTopic($idTopic){
+  global $conn;
 
+  $stmt = $conn->prepare("SELECT * FROM post WHERE topic_id = ?");
+  $stmt->execute(array($idTopic));
+  return $stmt->fetchAll();
+}
 
+function getCommentsOfPost($idPost){
+  global $conn;
 
+  $stmt = $conn->prepare("SELECT *  FROM comment WHERE post_id = ?");
+  $stmt->execute(array($idPost));
+  return $stmt->fetchAll();
+}
 
+function addComment($message, $date, $post_id, $commenter){
+  global $conn;
 
+  $stmt = $conn->prepare("INSERT INTO comment (message, date, post_id, commenter) VALUES (?,?,?,?)");
+  $stmt->execute(array($message, $date, $post_id, $commenter));
+
+}
+
+function addPost($content, $date, $topic_id, $poster){
+  global $conn;
+
+  $stmt = $conn->prepare("INSERT INTO post (content, date, topic_id, poster) VALUES (?,?,?,?)");
+  $stmt->execute(array($content, $date, $topic_id, $poster));
+
+}
+
+function addTopic($title, $project_id, $task_id, $type){
+  global $conn;
+
+  $stmt = $conn->prepare("INSERT INTO topic (title, project_id, task_id, TYPE) VALUES (?,?,?,?)");
+  $stmt->execute(array($title, $project_id, $task_id, $type));
+
+}
 
 ?>
