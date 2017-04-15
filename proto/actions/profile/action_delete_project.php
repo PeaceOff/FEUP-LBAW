8827d46002a949f7ddf448f7d9856c9bd45a999a
Grupo 65 de LBAW <lbaw1665@gnomo.fe.up.pt>
2017-04-15 15:01:15 +0100

    Added delete project action

---
+++ proto/actions/profile/action_delete_project.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+	include_once('../../config/init.php');
+	include_once($BASE_DIR . 'database/project.php');
+
+	if(!isset($_SESSION['username']){
+		header('Location: ../authentication/home.php');
+		exit();
+	}
+
+	$username = $_SESSION['username'];
+	$project_id = $_POST['project_id'];
+	
+	if(!project_allowed($username,$project_id)){
+		header('Location:../authentication/home.php');
+		exit();
+	}
+
+	project_delete($project_id);
+
+	header('Location:../pages/profile/personalPage.php');
+	exit();
+?>
