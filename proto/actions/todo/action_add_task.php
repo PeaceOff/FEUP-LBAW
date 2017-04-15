5be18fc959dd82106357af7ed64dea13871d2b4e
mferreira96 <pount@sapo.pt>
2017-04-15 15:32:12 +0100

    added action to add a task

--- proto/actions/todo/action_add_task.php
+++ proto/actions/todo/action_add_task.php
@@ -1,4 +1,22 @@
 <?php
 
+	include_once('../../config/init.php');
+	include_once($BASE_DIR . 'database/todo.php');
+
+	if(!isset($_SESSION['username'])){
+        header('Location: ../authentication/home.php');
+        exit();
+    }
+
+	$task_title = $_POST['title'];
+	$task_category = $_POST['category'];
+	$task_deadline = $_POST['deadline'];
+	$task_description =  $_POST['description'];
+
+	$project_id = $_GET['project_id'];
+
+  add_task($task_title,$task_description,$task_deadline, $_SESSION['username'], $project_id ,$task_category);
+
+	header('Location: ../../pages/todo/todoPage.php');
 
 ?>
