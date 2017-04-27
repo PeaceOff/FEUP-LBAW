2c7f50ffd4213a086164ec408ebebcce5b089a3f
LBAW1665 <lbaw1665@fe.up.pt>
2017-04-28 00:07:54 +0100

    added database statistics

--- final/javascript/statistics.js
+++ final/javascript/statistics.js
@@ -8,6 +8,18 @@ $(document).ready(function(){
     original.call(this, e, legendItem);
   };
 
+  var statistics;
+
+  $.ajax({
+  	type: "post",
+	dataType:"json",
+	async:false,
+	url:'../../api/get_statistics.php',
+	}).done(function(data){	
+	 	 statistics = data;	 
+  	});	
+
+
   var myChart = new Chart(ctx, {
     type: 'horizontalBar',
     data: {
@@ -15,19 +27,19 @@ $(document).ready(function(){
       datasets: [{
         label: 'Projects',
         backgroundColor: "rgba(65,78,91,1)",
-        data: [18],
+        data: [statistics.own_proj_number],
       }, {
         label: 'Collaborations',
         backgroundColor: "rgba(236,177,36,1)",
-        data: [30],
+        data: [statistics.collab_proj_number],
       },{
         label: 'Tasks Finished',
         backgroundColor: "rgba(20,183,90,1)",
-        data: [24],
+        data: [statistics.task_finished_number],
       }, {
         label: 'Tasks unfinished',
         backgroundColor: "rgba(34,34,34,1)",
-        data: [15],
+        data: [statistics.task_unfinished_number],
       }]
     },
     options: {
